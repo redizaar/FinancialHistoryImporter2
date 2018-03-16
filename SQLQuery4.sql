6e9e633f6d60740dd61cc5399c447b179a94bd7b
redizaar <pwnpwn90@gmail.com>
2018-03-16 22:53:35 +0100

    Add files via upload

---
+++ SQLQuery4.sql
@@ -0,0 +1,10 @@
+﻿CREATE PROC insertStockDataToSql
+@name varchar(50),
+@date varchar(50),
+@openprice float,
+@highprice float,
+@lowprice float,
+@closeprice float
+AS
+	INSERT INTO StockData.dbo.[Stock_WebData](Name,Date,openPrice,HighPrice,LowPrice,closePrice)
+	VALUES (@name,@date,@openprice,@highprice,@lowprice,@closeprice)
---
+++ SQLQuery5.sql
@@ -0,0 +1,11 @@
+﻿CREATE PROC insertNewColumns2
+@bankName varchar(50),
+@transStartRow int,
+@accountNumberPos varchar(50),
+@dateColumn varchar(50),
+@priceColumn varchar(50),
+@balanceColumn varchar(50),
+@commentColumn varchar(50)
+AS
+	INSERT INTO ImportFileData.dbo.[StoredColumns](BankName,TransStartRow,AccountNumberPos,DateColumn,PriceColumn,BalanceColumn,CommentColumn)
+	VALUES (@bankName,@transStartRow,@accountNumberPos,@dateColumn,@priceColumn,@balanceColumn,@commentColumn)
