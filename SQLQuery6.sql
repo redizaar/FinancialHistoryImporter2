3ddc309bb63dc6a73556dca007a84bf35cdb5904
redizaar <pwnpwn90@gmail.com>
2018-03-22 22:18:03 +0100

    Add files via upload

--- SQLQuery6.sql
+++ SQLQuery6.sql
@@ -1,7 +1,7 @@
 ﻿CREATE PROC registrationQuery3
 @username varchar(50),
 @password varchar(50),
-@accountnumber varchar(50)
+@accountnumber varchar(1000)
 AS
 	INSERT INTO LoginDB.dbo.[UserDatas](Username,Password,AccountNumber)
 	VALUES (@username,@password,@accountnumber)
