0a0ab3084fc8d766e51d14db7f28d53964b91f89
redizaar <pwnpwn90@gmail.com>
2018-04-03 23:51:09 +0200

    Add files via upload

---
+++ insertNewStockColumns2.sql
@@ -0,0 +1,11 @@
+﻿CREATE PROC insertNewStockColumns
+@bankName varchar(50),
+@transStartRow int,
+@stockName varchar(100),
+@priceColumn varchar(50),
+@quantityColumn varchar(50),
+@dateColumn varchar(50),
+@typeColumn varchar(50)
+AS
+	INSERT INTO ImportFileData.dbo.[StoredColumns_Stock](BankName,TransStartRow,StockName,PriceColumn,QuantityColumn,DateColumn,TypeColumn)
+	VALUES (@bankName,@transStartRow,@stockName,@priceColumn,@quantityColumn,@dateColumn,@typeColumn)
